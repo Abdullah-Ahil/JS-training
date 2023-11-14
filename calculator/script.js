e21f9c4075ab4abc4f058edcc4f5ff91028300bc
Abdullah-Ahil <ahilabdullah111@gmail.com>
2023-11-14 21:33:54 +0530

    calci

---
+++ calculator/script.js
@@ -0,0 +1,12 @@
+let screen  = document.getElementById('inputscr');
+let buttons = document.querySelectorAll('button');
+
+let string = '';
+
+
+let arr = Array.from(buttons);
+arr.map(button =>{
+    button.addEventListener('click',(val)=>{
+        if(val.target.innerHtml == '=')
+    })
+})
