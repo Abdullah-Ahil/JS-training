bd1576fa91fee8edc212e6b2e7d0ef8512daac27
Abdullah-Ahil <ahilabdullah111@gmail.com>
2023-11-16 20:28:44 +0530

    calci 3

--- calculator/script.js
+++ calculator/script.js
@@ -1,12 +1,30 @@
-let screen  = document.getElementById('inputscr');
+let screen  = document.querySelector('#inputscr');
 let buttons = document.querySelectorAll('button');
 
 let string = '';
 
 
 let arr = Array.from(buttons);
-arr.map(button =>{
+arr.forEach(button =>{
     button.addEventListener('click',(val)=>{
-        if(val.target.innerHtml == '=')
+        if(val.target.innerHtml == '='){
+            string = eval(string);
+
+            screen.value = string;
+        }
+        else if(val.target.innerHtml == 'AC'){
+            string = "";
+            screen.value = string;
+        }
+        else if(val.target.innerHtml == 'DEL'){
+            string = string.substring(0,string.length-1)
+            screen.value = string;
+        }
+        else{
+            string += val.target.innerHtml;
+            screen.value = string;
+
+        }
     })
 })
+// screen.value = 'hi'
